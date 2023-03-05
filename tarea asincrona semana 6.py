faf44a54dbd1618d0d7b64d49f62720a9fda5ce4
luismi1707 <as0939032023@unab.edu.sv>
2023-03-05 01:45:46 -0600

    Nuevo proyecto

---
+++ tarea asincrona semana 6.py
@@ -0,0 +1,52 @@
+print('Esta es la tarea de programacion de la semana 6')
+print('suma: ')
+numUno = 3
+numDos = 6
+numTres = 9
+numCuatro = numUno + numDos + numTres 
+resultado = numCuatro
+print('El resultado de la suma es:', resultado)
+'Al sumar las tres variables da el valor de la cuarta'
+
+print('resta: ')
+NumUno = 2
+NumDos = 4
+NumTres = 6
+NumCuatro = 8
+resultado = NumUno - NumDos - NumTres - NumCuatro
+print('El resultado de la  resta es:', resultado) 
+'La varibale NumTres es mayor que NumUno'
+
+print('Multiplicacion: ')
+num_uno = 4
+num_dos = 6
+variable1 = num_uno * num_dos
+resultado = variable1 * numDos * NumDos
+print('El resultado de la multiplicacion es:', resultado)
+'''La resultado de la multiplicacion sale de la multiplicacion 
+de la variable1 con las segundas variables de la suma y resta'''
+
+print('Exponente: ')
+Expo = num_uno ** num_dos
+resultado = Expo
+print('El exponente de la multiplicacion es:', resultado)
+'La variable num_uno se eleva a la variable num_dos'
+
+print('Modulo o residuo: ')
+resiDuo = numUno % NumUno
+resultado = resiDuo
+print('El residuo es:', resultado)
+'El residuo es lo que sobra de numUno y NumUno'
+
+print('division: ')
+Divi = resiDuo / Expo
+resultado = Divi
+print ('El total de la division es:', resultado)
+'Divido la variable resiDuo entre la variable Expo'
+
+print('division entera: ')
+number1 = resiDuo
+number2 = Expo
+resultado = resiDuo // Expo
+print('El resultado de la division entera es:', resultado)
+"Se hace lo mismo que en el anterior, pero aca dara un el resultado redondeado"
